345c1b2059aaeb2f07a2389a9c3679dcbd3d84d7
Ilya Fabiyanskiy <ilya.fabiyanskiy@yandex.ru>
2023-02-23 11:51:17 +0300

    kitty_gram

---
+++ kittygram/urls.py
@@ -0,0 +1,8 @@
+from django.urls import include, path
+
+from cats.views import CatList, CatDetail
+
+urlpatterns = [
+    path('cats/', CatList.as_view()),
+    path('cats/<int:pk>/', CatDetail.as_view()),
+] 
